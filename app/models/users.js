b5711306c809cf4c5ee5b495b3b9753501771fe8
MorenaBarboni <morena.barboni@studenti.unicam.it>
2019-12-09 09:42:37 +0100

    User Schema

---
+++ app/models/users.js
@@ -0,0 +1,70 @@
+var mongoose = require("mongoose");
+var crypto = require("crypto");
+var jwt = require("jsonwebtoken");
+var auth = 'testSecret';
+
+var userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  username: {
+    type: String
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  surname: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['Municipality', 'Police', 'ParkingCompany'],
+    required: true
+  },
+  company: {
+    type: String,
+    default: null
+  },
+  //Hash and salt generated from password
+  hash: String,
+  salt: String
+},
+  {
+    versionKey: false
+  });
+
+  userSchema.methods.setPassword = function (password) {
+    this.salt = crypto.randomBytes(16).toString("hex");
+    this.hash = crypto
+      .pbkdf2Sync(password, this.salt, 1000, 64, "sha1")
+      .toString("hex");
+  };
+  
+  userSchema.methods.verifyPassword = function (password) {
+    var hash = crypto
+      .pbkdf2Sync(password, this.salt, 1000, 64, "sha1")
+      .toString("hex");
+    return this.hash === hash;
+  };
+  
+  userSchema.methods.generateToken = function () {
+    var expiry = new Date();
+    expiry.setDate(expiry.getDate() + 7);
+  
+    return jwt.sign(
+      {
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        role: this.role,
+        exp: parseInt(expiry.getTime() / 1000)
+      },
+      auth
+    );
+  };
+
+mongoose.model("User", userSchema);
