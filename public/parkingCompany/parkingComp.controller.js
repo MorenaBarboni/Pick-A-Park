4512ec6d54a6008c63574700f132e61f3c4d3853
MorenaBarboni <morena.barboni@studenti.unicam.it>
2019-12-26 10:05:56 +0100

    Added method to retrieve unapproved parkings

--- public/parkingCompany/parkingComp.controller.js
+++ public/parkingCompany/parkingComp.controller.js
@@ -2,17 +2,64 @@
 
     angular.module("pick-a-park").controller("parkingCompCtrl", parkingCompCtrl);
 
-    parkingCompCtrl.$inject = ["$http", "$window", "$location", "$scope", "authentication"];
-    function parkingCompCtrl($http, $window, $location, $scope, authentication) {
+    parkingCompCtrl.$inject = ["$http", "$window", "$location", "$scope", "authentication", "parkingService"];
+    function parkingCompCtrl($http, $window, $location, $scope, authentication, parkingService) {
 
         var vm = this;
 
+        vm.user = {}; //Current user data
+        vm.unapprovedParkings = []
 
+
+        vm.newParking = {
+            id: null,
+            city: "",
+            address: "",
+            longitude: null,
+            latitude: null,
+            price: null,
+            indoor: false,
+            handicap: false
+        };
 
         initController();
 
         function initController() {
+            authentication.getUser()
+                .success(function (data) {
+                    vm.user = data;
+                })
+                .error(function (e) {
+                    console.log(e);
+                }).then(function () {
+                    getUnapprovedParkings()
+                })
+        }
 
+        //Request new parking
+        vm.onSubmitRequest = function () {
+            parkingService.newParking(vm.user.company, vm.newParking).then(function (response) {
+                if (response === "existingParkingError") {
+                    window.alert("L'ID selezionato è già esistente");
+                } else if (response === "existingCoordError") {
+                    window.alert("Le coordinate selezionate sono già esistenti");
+                } else {
+                    window.alert("Richiesta inviata con successo");
+                    window.location.reload();
+                }
+            });
+        };
+
+        function getUnapprovedParkings() {
+            parkingService
+                .getParkings(vm.user.company)
+                .then(function (result) {
+                    result.forEach(parking => {
+                        if (!parking.isApproved) {
+                            vm.unapprovedParkings.push(parking);
+                        }
+                    });
+                });
         }
 
 
